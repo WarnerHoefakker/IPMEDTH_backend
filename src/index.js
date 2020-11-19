d2222efe637292c9c26be28589a435cc5392e06c
Anouk <aananouk@gmail.com>
2020-11-19 12:05:49 +0100

    Removed test route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const testRouter = require('./routers/test');
+// const testRouter = require('./routers/test');
 
 const app = express();
 app.use(express.json());
-app.use(testRouter);
+// app.use(testRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server is up on port ', process.env.PORT || 3000)
