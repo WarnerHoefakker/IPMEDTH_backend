600db204ca7a7559fb70cd69d38153c17323762b
Anouk <aananouk@gmail.com>
2020-11-19 10:47:23 +0100

    Test routes

--- src/routers/test.js
+++ src/routers/test.js
@@ -5,5 +5,12 @@ router.get('/test', (req, res) => {
     res.send('test');
 });
 
+router.get('/', (req, res) => {
+    res.send('home')
+});
+
+router.get('/123', (req, res) => {
+    res.send('123')
+});
 
 module.exports = router;
