63627cdb9e4d7db4c268be32e0770a21e6be0d71
Warner Hoefakker <warner.hoefakker@live.nl>
2020-11-23 13:25:30 +0100

    add post request for RFID module
    --

--- src/routers/rfid.js
+++ src/routers/rfid.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = new express.Router();
+const Level = require('../models/level');
+const RFID = require('../models/rfid');
 
 router.get('/rfid', (req, res) => {
     res.send({tag: 'id123'});
+});
+
+router.post('/rfid-add', async (req, res) => {
+    const { value } = req.body;
+
+    const newValue = new RFID({value: value,roomid:0});
+
+    await newValue.save();
+    // const level = await Co2.find({});
+
+    console.log(value);
+    res.send({value: 894353434});
 });
 
 module.exports = router;
