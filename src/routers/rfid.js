6cf815645367cf7c854a79c741d1541e2e65a7fc
Warner Hoefakker <warner.hoefakker@live.nl>
2020-11-29 15:03:44 +0100

    --

--- src/routers/rfid.js
+++ src/routers/rfid.js
@@ -9,7 +9,7 @@ router.get('/rfid', (req, res) => {
 
 router.post('/rfidadd', async (req, res) => {
     const { value } = req.body;
-
+    console.log(req.body);
     const newValue = new RFID({value: value, roomid: value});
 
     await newValue.save();
